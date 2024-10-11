74e8e40eed11b78e2e97adf701082eb5243ffa61
blakeaw <blakeaw1102@gmail.com>
2024-10-11 10:15:15 -0500

    Updated some parameters in the model. Renamed the CANCER compartment to TUMOR.

--- rltmod/rlt_model.py
+++ rltmod/rlt_model.py
@@ -23,11 +23,11 @@ with units():
     SimulationUnits(time="h", concentration="nM", volume="L")
 
     ##  Compartments  ##
-    Parameter("V_CENTRAL", 15.0, unit="L")
+    Parameter("V_CENTRAL", 5.0, unit="L")
     Parameter("V_PERIPHERAL", 10.0, unit="L")
-    Parameter("V_CANCER", 0.5, unit="L")
+    Parameter("V_TUMOR", 0.5, unit="L")
     pkpd.three_compartments(
-        "CENTRAL", V_CENTRAL, "PERIPHERAL", V_PERIPHERAL, "CANCER", V_CANCER
+        "CENTRAL", V_CENTRAL, "PERIPHERAL", V_PERIPHERAL, "TUMOR", V_TUMOR
     )
 
     # Radioligand therapy
@@ -43,7 +43,7 @@ with units():
     # Target Biomarker
     Monomer("Biomarker", ["b", "endo"], {"endo": ["T", "F"]})
     Parameter("Biomarker_0", 100.0, unit="nM")
-    Initial(Biomarker(b=None, endo="F") ** CANCER, Biomarker_0)
+    Initial(Biomarker(b=None, endo="F") ** TUMOR, Biomarker_0)
 
     # RLT decay
     Parameter("rlt_halflife", 2.67, unit="day")  # Yttrium-90 t_1/2 = 2.67 days
@@ -68,38 +68,38 @@ with units():
     )
     Rule(
         "rlt_decay_cancer_free",
-        RLT(b=None, decay="F", endo="F") ** CANCER
-        >> RLT(b=None, decay="T", endo="F") ** CANCER
-        + Emission(endo="F", bound="F") ** CANCER,
+        RLT(b=None, decay="F", endo="F") ** TUMOR
+        >> RLT(b=None, decay="T", endo="F") ** TUMOR
+        + Emission(endo="F", bound="F") ** TUMOR,
         k_rlt_decay,
     )
     Rule(
         "rlt_decay_cancer_bound",
-        RLT(b=ANY, decay="F", endo="F") ** CANCER
-        >> RLT(b=ANY, decay="T", endo="F") ** CANCER
-        + Emission(endo="F", bound="T") ** CANCER,
+        RLT(b=ANY, decay="F", endo="F") ** TUMOR
+        >> RLT(b=ANY, decay="T", endo="F") ** TUMOR
+        + Emission(endo="F", bound="T") ** TUMOR,
         k_rlt_decay,
     )
     Rule(
         "rlt_decay_cancer_bound_endo",
-        RLT(b=ANY, decay="F", endo="T") ** CANCER
-        >> RLT(b=ANY, decay="T", endo="T") ** CANCER
-        + Emission(endo="T", bound="T") ** CANCER,
+        RLT(b=ANY, decay="F", endo="T") ** TUMOR
+        >> RLT(b=ANY, decay="T", endo="T") ** TUMOR
+        + Emission(endo="T", bound="T") ** TUMOR,
         k_rlt_decay,
     )
     # Leaving the generated Emission as endo=T and bound=T since
     # it was generated after binding.
     Rule(
         "rlt_decay_cancer_endo",
-        RLT(b=None, decay="F", endo="T") ** CANCER
-        >> RLT(b=None, decay="T", endo="T") ** CANCER
-        + Emission(endo="T", bound="T") ** CANCER,
+        RLT(b=None, decay="F", endo="T") ** TUMOR
+        >> RLT(b=None, decay="T", endo="T") ** TUMOR
+        + Emission(endo="T", bound="T") ** TUMOR,
         k_rlt_decay,
     )
     ##  Drug Distribution  ##
     # CENTRAL <-> PERIPHERAL
     Parameter("kf_distribute_RLT_CENTRAL_PERIPHERAL", 0.1, unit=" 1 / h")
-    Parameter("kr_distribute_RLT_CENTRAL_PERIPHERAL", 0.1, unit=" 1 / h")
+    Parameter("kr_distribute_RLT_CENTRAL_PERIPHERAL", 0.01, unit=" 1 / h")
     pkpd.distribute(
         RLT(b=None, endo="F"),
         CENTRAL,
@@ -109,35 +109,35 @@ with units():
             kr_distribute_RLT_CENTRAL_PERIPHERAL,
         ],
     )
-    # CENTRAL <-> CANCER
-    Parameter("kf_distribute_RLT_CENTRAL_CANCER", 0.05, unit=" 1 / h")
-    Parameter("kr_distribute_RLT_CENTRAL_CANCER", 0.05, unit=" 1 / h")
+    # CENTRAL <-> TUMOR
+    Parameter("kf_distribute_RLT_CENTRAL_TUMOR", 0.05, unit=" 1 / h")
+    Parameter("kr_distribute_RLT_CENTRAL_TUMOR", 0.01, unit=" 1 / h")
     pkpd.distribute(
         RLT(b=None, endo="F"),
         CENTRAL,
-        CANCER,
-        klist=[kf_distribute_RLT_CENTRAL_CANCER, kr_distribute_RLT_CENTRAL_CANCER],
+        TUMOR,
+        klist=[kf_distribute_RLT_CENTRAL_TUMOR, kr_distribute_RLT_CENTRAL_TUMOR],
     )
 
     ##  Biomarker Binding  ##
     Parameter("kr_bind_rlt_biomarker", 1e-3, unit="1/s")
-    Parameter("Kd_rlt_biomarker", 10.0, unit="nM")
+    Parameter("Kd_rlt_biomarker", 100.0, unit="nM")
     Expression("kf_bind_rlt_biomarker", kr_bind_rlt_biomarker / Kd_rlt_biomarker)
     # Binding between extracellular RLT (endo="F") and membrane-bound biomarker (endo="F")
     macros.bind(
-        RLT(endo="F") ** CANCER,
+        RLT(endo="F") ** TUMOR,
         "b",
-        Biomarker(endo="F") ** CANCER,
+        Biomarker(endo="F") ** TUMOR,
         "b",
         [kf_bind_rlt_biomarker, kr_bind_rlt_biomarker],
     )
 
     ## Endocytosis of bound biomarker ##
-    Parameter("k_endo", 1e-4, unit="1/s")
+    Parameter("k_endo", 1e-1, unit="1/s")
     Rule(
         "biomarker_endocytosis",
-        RLT(b=ANY, endo="F") % Biomarker(b=ANY, endo="F") ** CANCER
-        >> RLT(b=ANY, endo="T") % Biomarker(b=ANY, endo="T") ** CANCER,
+        RLT(b=ANY, endo="F") % Biomarker(b=ANY, endo="F") ** TUMOR
+        >> RLT(b=ANY, endo="T") % Biomarker(b=ANY, endo="T") ** TUMOR,
         k_endo,
     )
 
@@ -145,16 +145,16 @@ with units():
     Parameter("k_unbind", 1e-2, unit="1 / s")
     Rule(
         "endocytic_unbinding",
-        RLT(b=1, endo="T") % Biomarker(b=1, endo="T") ** CANCER
-        >> RLT(b=None, endo="T") + Biomarker(b=None, endo="T") ** CANCER,
+        RLT(b=1, endo="T") % Biomarker(b=1, endo="T") ** TUMOR
+        >> RLT(b=None, endo="T") + Biomarker(b=None, endo="T") ** TUMOR,
         k_unbind,
     )
 
     ## Biomarker recycling
-    Parameter("k_biomarker_recycle", 1e-4, unit="1 / s")
+    Parameter("k_biomarker_recycle", 1e-3, unit="1 / s")
     Rule(
         "biomarker_recyle",
-        Biomarker(b=None, endo="T") ** CANCER >> Biomarker(b=None, endo="F") ** CANCER,
+        Biomarker(b=None, endo="T") ** TUMOR >> Biomarker(b=None, endo="F") ** TUMOR,
         k_biomarker_recycle,
     )
 
@@ -166,19 +166,21 @@ with units():
     # Total RLT in each compartment
     Observable("obs_RLT_CENTRAL", RLT() ** CENTRAL)
     Observable("obs_RLT_PERIPHERAL", RLT() ** PERIPHERAL)
-    Observable("obs_RLT_CANCER", RLT(b=WILD, endo="F") ** CANCER)
+    Observable("obs_RLT_TUMOR", RLT(b=WILD, endo="F") ** TUMOR)
     # Radioactive RLT in each compartment
     Observable("obs_RLT_CENTRAL_active", RLT(b=None, decay="F", endo="F") ** CENTRAL)
     Observable(
         "obs_RLT_PERIPHERAL_active", RLT(b=None, decay="F", endo="F") ** PERIPHERAL
     )
-    Observable("obs_RLT_CANCER_active", RLT(b=WILD, decay="F") ** CANCER)
+    Observable("obs_RLT_TUMOR_active", RLT(b=WILD, decay="F") ** TUMOR)
 
     # Decayed RLT in each compartment
     Observable("obs_RLT_CENTRAL_decay", RLT(b=None, decay="T") ** CENTRAL)
     Observable("obs_RLT_PERIPHERAL_decay", RLT(b=None, decay="T") ** PERIPHERAL)
-    Observable("obs_RLT_CANCER_decay_exo", RLT(b=WILD, decay="T", endo="F") ** CANCER)
+    Observable("obs_RLT_TUMOR_decay_exo", RLT(b=WILD, decay="T", endo="F") ** TUMOR)
 
+    # RLT in the TUMOR compartment that has been internalized
+    Observable("obs_RLT_TUMOR_endo", RLT(endo="T") ** TUMOR)
     # Emissions in each compartment and state
     # All emissions
     Observable("obs_Emissions", Emission())
@@ -193,22 +195,22 @@ with units():
             + (obs_Emission_PERIPHERAL * V_PERIPHERAL) / (V_CENTRAL + V_PERIPHERAL)
         ),
     )
-    # Emissions in the CANCER compartment
-    Observable("obs_Emission_CANCER", Emission() ** CANCER)
-    # Emissions in the CANCER compartment from RLT bound to the biomarker
-    Observable("obs_Emission_CANCER_bound", Emission(bound="T", endo="F") ** CANCER)
-    # Emissions in the CANCER compartment from RLT bound to biomarker that
+    # Emissions in the TUMOR compartment
+    Observable("obs_Emission_TUMOR", Emission() ** TUMOR)
+    # Emissions in the TUMOR compartment from RLT bound to the biomarker
+    Observable("obs_Emission_TUMOR_bound", Emission(bound="T", endo="F") ** TUMOR)
+    # Emissions in the TUMOR compartment from RLT bound to biomarker that
     # has undergone endocytosis or from RLT that was endocytosed but unbound
     # from the biomarker.
     Observable(
-        "obs_Emission_CANCER_bound_endo", Emission(endo="T", bound="T") ** CANCER
+        "obs_Emission_TUMOR_bound_endo", Emission(endo="T", bound="T") ** TUMOR
     )
-    # Emissions in the CANCER compartment from RLT bound to biomarker that
+    # Emissions in the TUMOR compartment from RLT bound to biomarker that
     # has undergone endocytosis
     Expression(
         "obs_Targeted_Emissions",
-        (obs_Emission_CANCER_bound + obs_Emission_CANCER_bound_endo),
+        (obs_Emission_TUMOR_bound + obs_Emission_TUMOR_bound_endo),
     )
     # Biomarker
-    Observable("obs_exo_Biomarker", Biomarker(endo="F") ** CANCER)
-    Observable("obs_endo_Biomarker", Biomarker(endo="T") ** CANCER)
+    Observable("obs_exo_Biomarker", Biomarker(endo="F") ** TUMOR)
+    Observable("obs_endo_Biomarker", Biomarker(endo="T") ** TUMOR)
